74658f75a4cf72a261bc2d9711a88ba03cdeaae0
Andrew Cohn <andrewcohn0@gmail.com>
2018-09-22 12:26:49 -0400

    initial pokemon review code

--- 31-pokemon-search-review/src/index.js
+++ 31-pokemon-search-review/src/index.js
@@ -1,4 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // log all pokemon data
   console.log(POKEMON)
   console.log('%c FIRST POKEMON: ', 'color: purple', POKEMON[0])
+
+// find HTML elements we care about
+  const pokemonContainerToAppend = document.querySelector('#pokemon-container')
+  const pokemonSearchInputField = document.querySelector('#pokemon-search-input')
+
+  // POKEMON.forEach((pokemonJSONObject) => {
+  //   const pTagForPokemon = document.createElement('p')
+  //   pTagForPokemon.innerText = pokemonJSONObject.name
+  //   pokemonContainerToAppend.appendChild(pTagForPokemon)
+  // })
+// render all pokemon to the page when our app first mounts
+  const pokemonHTMLString = POKEMON.map((pokemonJSONObject) => `<p>${pokemonJSONObject.name}</p>`)
+  pokemonContainerToAppend.innerHTML = pokemonHTMLString.join('')
+
+  pokemonSearchInputField.addEventListener('input', (event) => {
+    const filteredPokemonBasedOnSearchTerm = POKEMON.filter((pokemonJSONObject) => {
+      // does the current pokemon in our itereration have a name that INCLUDES whatever the user typed
+      return pokemonJSONObject.name.includes(event.target.value)
+    })
+    const filteredPokemonHTMLString = filteredPokemonBasedOnSearchTerm.map((pokemonJSONObject) =>
+     `<p>${pokemonJSONObject.name}</p>`)
+
+    pokemonContainerToAppend.innerHTML = filteredPokemonHTMLString.join('')
+  })
 })
